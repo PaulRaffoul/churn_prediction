942004767c3c4abad69096e5644b29072b253093
PaulRaffoul <paul_raffoul24@hotmail.com>
2023-12-27 20:40:36 +0200

    data Ingestion

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,46 @@
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+class DataIngestion:
+    def __init__(self):
+        self.train_data_path = os.path.join('artifacts','train.xlsx')
+        self.test_data_path = os.path.join('artifacts','test.xlsx')
+        self.raw_data_path = os.path.join('artifacts','data.xlsx')
+
+    def initiate_data_ingestion(self):
+        logging.info('enter the data ingestion component')
+        try:
+            df = pd.read_excel('data\Telco_customer_churn.xlsx')
+            logging.info('Read the data')
+
+            #create the train data folder
+            os.makedirs(os.path.dirname(self.train_data_path),exist_ok=True)
+
+            df.to_excel(self.raw_data_path, index=False,header=True)
+
+            logging.info('Train test split started')
+            train_set,test_set = train_test_split(df,test_size=0.2,random_state=42,stratify=df['Churn Value'])
+
+            train_set.to_excel(self.train_data_path, index=False,header=True)
+            test_set.to_excel(self.test_data_path, index=False,header=True)
+
+            logging.info('Data split ended')
+
+            return(
+                self.train_data_path,
+                self.test_data_path,
+            )
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+if __name__=='__main__':
+    
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+
+    
